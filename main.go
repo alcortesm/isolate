a995d487291606c7c23c50e18110ddccf0156cb7
Alberto Cortés <alcortesm@gmail.com>
2017-08-24 13:01:57 +0200

    add uts isolation

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ const usage = `Runs the specified command (and its arguments) with configurable
 levels of isolation.
 
 The standard input, output and error of the isolated command
-are redirected to the ones of the "isolate" command.
+are redirected to the ones of the main "isolate" process.
 
 Usage:
 	isolate [options] command [arguments]
@@ -32,10 +32,14 @@ func main() {
 		flag.PrintDefaults()
 	}
 	showExitCode := flag.Bool("exitCode", false,
-		"prints the exit code of the isolated command to stdout")
+		"prints the exit code of the isolated command to stdout.")
+	opts := runOpts{
+		newUTS: flag.Bool("uts", false,
+			"run the isolated command in a new UTS namespace, initialized after the one of the main process."),
+	}
 	flag.Parse()
 
-	exitCode, err := run(flag.Args())
+	exitCode, err := run(flag.Args(), opts)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -45,7 +49,11 @@ func main() {
 	}
 }
 
-func run(words []string) (int, error) {
+type runOpts struct {
+	newUTS *bool
+}
+
+func run(words []string, opts runOpts) (int, error) {
 	if len(words) == 0 {
 		return 0, fmt.Errorf("missing command")
 	}
@@ -53,6 +61,11 @@ func run(words []string) (int, error) {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = new(syscall.SysProcAttr)
+	if *opts.newUTS {
+		// requires Linux >= 2.6.19 and CAP_SYS_ADMIN
+		cmd.SysProcAttr.Cloneflags = syscall.CLONE_NEWUTS
+	}
 	return unixExitCodeOrError(cmd.Run())
 }
 
